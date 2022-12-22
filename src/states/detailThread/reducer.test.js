b4fa864563b80214ee05c5d7fc04536f7742bee8
fadhilrobbani <rabbanifadhillailham@gmail.com>
2022-12-23 03:41:27 +0700

    finish detailThreadReducer test

--- src/states/detailThread/reducer.test.js
+++ src/states/detailThread/reducer.test.js
@@ -203,4 +203,384 @@ describe('detailThreadReducer function', () => {
       downVotesBy: [action.payload.authUserId],
     });
   });
+
+  it('should return the detailThread with new downVotesBy and empty upVotesBy if upVotesBy already upvote by user and downVotesBy are empty when given by detailThread/downVotes action', () => {
+    const initialState = {
+      id: 'thread-1',
+      title: 'Thread Pertama',
+      body: 'Ini adalah thread pertama',
+      category: 'General',
+      createdAt: '2021-06-21T07:00:00.000Z',
+      owner: {
+        id: 'users-1',
+        name: 'John Doe',
+        avatar: 'https://generated-image-url.jpg',
+      },
+      upVotesBy: ['users-1'],
+      downVotesBy: [],
+      comments: [
+        {
+          id: 'comment-1',
+          content: 'Ini adalah komentar pertama',
+          createdAt: '2021-06-21T07:00:00.000Z',
+          owner: {
+            id: 'users-1',
+            name: 'John Doe',
+            avatar: 'https://generated-image-url.jpg',
+          },
+          upVotesBy: [],
+          downVotesBy: [],
+        },
+      ],
+    };
+    const action = {
+      type: ActionType.DOWN_VOTES_DETAIL_THREADS,
+      payload: {
+        threadId: 'thread-1',
+        authUserId: 'users-1',
+      },
+    };
+
+    const nextState = detailThreadReducer(initialState, action);
+    expect(nextState).toStrictEqual({
+      ...initialState,
+      upVotesBy: [],
+      downVotesBy: [action.payload.authUserId],
+    });
+  });
+
+  it('should return the detailThread with empty upVotesBy and downVotesBy  when given by detailThread/neutralVotes action', () => {
+    const initialState = {
+      id: 'thread-1',
+      title: 'Thread Pertama',
+      body: 'Ini adalah thread pertama',
+      category: 'General',
+      createdAt: '2021-06-21T07:00:00.000Z',
+      owner: {
+        id: 'users-1',
+        name: 'John Doe',
+        avatar: 'https://generated-image-url.jpg',
+      },
+      upVotesBy: ['users-1'],
+      downVotesBy: ['users-1'],
+      comments: [
+        {
+          id: 'comment-1',
+          content: 'Ini adalah komentar pertama',
+          createdAt: '2021-06-21T07:00:00.000Z',
+          owner: {
+            id: 'users-1',
+            name: 'John Doe',
+            avatar: 'https://generated-image-url.jpg',
+          },
+          upVotesBy: [],
+          downVotesBy: [],
+        },
+      ],
+    };
+    const action = {
+      type: ActionType.NEUTRAL_VOTES_DETAIL_THREADS,
+      payload: {
+        threadId: 'thread-1',
+        authUserId: 'users-1',
+      },
+    };
+
+    const nextState = detailThreadReducer(initialState, action);
+    expect(nextState).toStrictEqual({
+      ...initialState,
+      upVotesBy: [],
+      downVotesBy: [],
+    });
+  });
+
+  it('should return the detailThread with new upVotesBy in comments property if upVotesBy and downVotesBy in comments property are empty when given by detailThread/upVotesComment action', () => {
+    const initialState = {
+      id: 'thread-1',
+      title: 'Thread Pertama',
+      body: 'Ini adalah thread pertama',
+      category: 'General',
+      createdAt: '2021-06-21T07:00:00.000Z',
+      owner: {
+        id: 'users-1',
+        name: 'John Doe',
+        avatar: 'https://generated-image-url.jpg',
+      },
+      upVotesBy: [],
+      downVotesBy: [],
+      comments: [
+        {
+          id: 'comment-1',
+          content: 'Ini adalah komentar pertama',
+          createdAt: '2021-06-21T07:00:00.000Z',
+          owner: {
+            id: 'users-1',
+            name: 'John Doe',
+            avatar: 'https://generated-image-url.jpg',
+          },
+          upVotesBy: [],
+          downVotesBy: [],
+        },
+      ],
+    };
+    const action = {
+      type: ActionType.UP_VOTES_COMMENT,
+      payload: {
+        threadId: 'thread-1',
+        commentId: 'comment-1',
+        authUserId: 'users-1',
+      },
+    };
+
+    const nextState = detailThreadReducer(initialState, action);
+    expect(nextState).toStrictEqual({
+      ...initialState,
+      comments: [
+        {
+          id: 'comment-1',
+          content: 'Ini adalah komentar pertama',
+          createdAt: '2021-06-21T07:00:00.000Z',
+          owner: {
+            id: 'users-1',
+            name: 'John Doe',
+            avatar: 'https://generated-image-url.jpg',
+          },
+          upVotesBy: ['users-1'],
+          downVotesBy: [],
+        },
+      ],
+    });
+  });
+
+  it('should return the detailThread with new upVotesBy in comments property if upVotesBy is empty and downVotesBy already downVotesBy in comments property when given by detailThread/upVotesComment action', () => {
+    const initialState = {
+      id: 'thread-1',
+      title: 'Thread Pertama',
+      body: 'Ini adalah thread pertama',
+      category: 'General',
+      createdAt: '2021-06-21T07:00:00.000Z',
+      owner: {
+        id: 'users-1',
+        name: 'John Doe',
+        avatar: 'https://generated-image-url.jpg',
+      },
+      upVotesBy: [],
+      downVotesBy: ['users-1'],
+      comments: [
+        {
+          id: 'comment-1',
+          content: 'Ini adalah komentar pertama',
+          createdAt: '2021-06-21T07:00:00.000Z',
+          owner: {
+            id: 'users-1',
+            name: 'John Doe',
+            avatar: 'https://generated-image-url.jpg',
+          },
+          upVotesBy: [],
+          downVotesBy: [],
+        },
+      ],
+    };
+    const action = {
+      type: ActionType.UP_VOTES_COMMENT,
+      payload: {
+        threadId: 'thread-1',
+        commentId: 'comment-1',
+        authUserId: 'users-1',
+      },
+    };
+
+    const nextState = detailThreadReducer(initialState, action);
+    expect(nextState).toStrictEqual({
+      ...initialState,
+      comments: [
+        {
+          id: 'comment-1',
+          content: 'Ini adalah komentar pertama',
+          createdAt: '2021-06-21T07:00:00.000Z',
+          owner: {
+            id: 'users-1',
+            name: 'John Doe',
+            avatar: 'https://generated-image-url.jpg',
+          },
+          upVotesBy: ['users-1'],
+          downVotesBy: [],
+        },
+      ],
+    });
+  });
+
+  it('should return the detailThread with new downVotesBy in comments property if upVotesBy and downVotesBy in comments property are empty when given by detailThread/downVotesComment action', () => {
+    const initialState = {
+      id: 'thread-1',
+      title: 'Thread Pertama',
+      body: 'Ini adalah thread pertama',
+      category: 'General',
+      createdAt: '2021-06-21T07:00:00.000Z',
+      owner: {
+        id: 'users-1',
+        name: 'John Doe',
+        avatar: 'https://generated-image-url.jpg',
+      },
+      upVotesBy: [],
+      downVotesBy: [],
+      comments: [
+        {
+          id: 'comment-1',
+          content: 'Ini adalah komentar pertama',
+          createdAt: '2021-06-21T07:00:00.000Z',
+          owner: {
+            id: 'users-1',
+            name: 'John Doe',
+            avatar: 'https://generated-image-url.jpg',
+          },
+          upVotesBy: [],
+          downVotesBy: [],
+        },
+      ],
+    };
+    const action = {
+      type: ActionType.DOWN_VOTES_COMMENT,
+      payload: {
+        threadId: 'thread-1',
+        commentId: 'comment-1',
+        authUserId: 'users-1',
+      },
+    };
+
+    const nextState = detailThreadReducer(initialState, action);
+    expect(nextState).toStrictEqual({
+      ...initialState,
+      comments: [
+        {
+          id: 'comment-1',
+          content: 'Ini adalah komentar pertama',
+          createdAt: '2021-06-21T07:00:00.000Z',
+          owner: {
+            id: 'users-1',
+            name: 'John Doe',
+            avatar: 'https://generated-image-url.jpg',
+          },
+          upVotesBy: [],
+          downVotesBy: ['users-1'],
+        },
+      ],
+    });
+  });
+
+  it('should return the detailThread with new downVotesBy in comments property if upVotesBy already upVotesBy by user and empty downVotesBy in comments property when given by detailThread/downVotesComment action', () => {
+    const initialState = {
+      id: 'thread-1',
+      title: 'Thread Pertama',
+      body: 'Ini adalah thread pertama',
+      category: 'General',
+      createdAt: '2021-06-21T07:00:00.000Z',
+      owner: {
+        id: 'users-1',
+        name: 'John Doe',
+        avatar: 'https://generated-image-url.jpg',
+      },
+      upVotesBy: ['users-1'],
+      downVotesBy: [],
+      comments: [
+        {
+          id: 'comment-1',
+          content: 'Ini adalah komentar pertama',
+          createdAt: '2021-06-21T07:00:00.000Z',
+          owner: {
+            id: 'users-1',
+            name: 'John Doe',
+            avatar: 'https://generated-image-url.jpg',
+          },
+          upVotesBy: [],
+          downVotesBy: [],
+        },
+      ],
+    };
+    const action = {
+      type: ActionType.DOWN_VOTES_COMMENT,
+      payload: {
+        threadId: 'thread-1',
+        commentId: 'comment-1',
+        authUserId: 'users-1',
+      },
+    };
+
+    const nextState = detailThreadReducer(initialState, action);
+    expect(nextState).toStrictEqual({
+      ...initialState,
+      comments: [
+        {
+          id: 'comment-1',
+          content: 'Ini adalah komentar pertama',
+          createdAt: '2021-06-21T07:00:00.000Z',
+          owner: {
+            id: 'users-1',
+            name: 'John Doe',
+            avatar: 'https://generated-image-url.jpg',
+          },
+          upVotesBy: [],
+          downVotesBy: ['users-1'],
+        },
+      ],
+    });
+  });
+
+  it('should return the detailThread with empty upVotesBy and downVotesBy in comments property when given by detailThread/neutralVotesComment action', () => {
+    const initialState = {
+      id: 'thread-1',
+      title: 'Thread Pertama',
+      body: 'Ini adalah thread pertama',
+      category: 'General',
+      createdAt: '2021-06-21T07:00:00.000Z',
+      owner: {
+        id: 'users-1',
+        name: 'John Doe',
+        avatar: 'https://generated-image-url.jpg',
+      },
+      upVotesBy: ['users-1'],
+      downVotesBy: ['users-1'],
+      comments: [
+        {
+          id: 'comment-1',
+          content: 'Ini adalah komentar pertama',
+          createdAt: '2021-06-21T07:00:00.000Z',
+          owner: {
+            id: 'users-1',
+            name: 'John Doe',
+            avatar: 'https://generated-image-url.jpg',
+          },
+          upVotesBy: [],
+          downVotesBy: [],
+        },
+      ],
+    };
+    const action = {
+      type: ActionType.NEUTRAL_VOTES_COMMENT,
+      payload: {
+        threadId: 'thread-1',
+        commentId: 'comment-1',
+        authUserId: 'users-1',
+      },
+    };
+
+    const nextState = detailThreadReducer(initialState, action);
+    expect(nextState).toStrictEqual({
+      ...initialState,
+      comments: [
+        {
+          id: 'comment-1',
+          content: 'Ini adalah komentar pertama',
+          createdAt: '2021-06-21T07:00:00.000Z',
+          owner: {
+            id: 'users-1',
+            name: 'John Doe',
+            avatar: 'https://generated-image-url.jpg',
+          },
+          upVotesBy: [],
+          downVotesBy: [],
+        },
+      ],
+    });
+  });
 });

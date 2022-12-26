7c54b7a6d6a9402f660b0d11bfa2c8c303dda786
Fadhilla Ilham Robbani <rabbanifadhillailham@gmail.com>
2022-12-26 20:26:38 +0700

    fix error login cypress error

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,18 +1,15 @@
 describe('Login spec', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/login');
+    cy.visit('http://localhost:3000/');
+    cy.get('footer span').contains('Login').click();
   });
   it('should display login page correctly', () => {
     cy.get('#email').should('be.visible');
     cy.get('input[placeholder="******"]').should('be.visible');
-    cy.get('button')
-      .contains(/^Login$/)
-      .should('be.visible');
+    cy.get('#loginUser').should('be.visible');
   });
   it('should display alert', () => {
-    cy.get('button')
-      .contains(/^Login$/)
-      .click();
+    cy.get('#loginUser').click();
     cy.on('window:alert', (str) => {
       expect(str).toBe('"email" is not allowed to be empty');
     });
@@ -20,9 +17,7 @@ describe('Login spec', () => {
   it('should display alert when password is empty', () => {
     cy.get('#email').type('testuser@email.com');
 
-    cy.get('button')
-      .contains(/^Login$/)
-      .click();
+    cy.get('#loginUser').click();
 
     cy.on('window:alert', (str) => {
       expect(str).to.equal('"password" is not allowed to be empty');
@@ -32,9 +27,7 @@ describe('Login spec', () => {
     cy.get('#email').type('testuser@email.com');
     cy.get('input[placeholder="******"]').type('salah');
 
-    cy.get('button')
-      .contains(/^Login$/)
-      .click();
+    cy.get('#loginUser').click();
 
     cy.on('window:alert', (str) => {
       expect(str).to.equal('email or password is wrong');
@@ -44,11 +37,9 @@ describe('Login spec', () => {
     cy.get('#email').type('kucingoren@gmail.com');
     cy.get('input[placeholder="******"]').type('kucingoren');
 
-    cy.get('button')
-      .contains(/^Login$/)
-      .click();
+    cy.get('#loginUser').click();
 
-    cy.get('footer span').contains('logout').should('be.visible');
+    cy.get('footer span').contains('Logout').should('be.visible');
     cy.get('#addThreadButton').should('be.visible');
   });
 });
